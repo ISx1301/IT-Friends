21c080b9d860e2e0a14b032e7002a0fbbe69d63a
DubblerPro <igorshkuro2000@gmail.com>
2025-07-02 20:01:20 +0300

    Add and configure Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,md,mdx,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
