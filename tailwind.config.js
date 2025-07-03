6fcbe761cab072a05f68fa0cca31acd8a997f12c
Ihor Savvov <ihor.savvov@fedevco.com>
2025-07-03 13:55:28 +0300

    Added TODOs; update global styles; changed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,43 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 module.exports = {
-  content: [
-    './src/**/*.{astro,html,js,md,mdx,ts}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: { // TODO: set your containers
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '992px',
+			xl: '1160px',
+		},
+		container: {
+			center: true,
+			padding: '1rem'			
+		},
+		colors: {
+			'white': '#ffffff',
+			'black': '#000000',
+			'red': {
+				DEFAULT: '#fd6150',
+				'error': '#e54230'
+			},
+		},
+		fontFamily: {
+			primary: ['Anek Latin', 'sans-serif', ...defaultTheme.fontFamily.sans], // TODO: include 2 fonts, https://transfonter.org/
+			// secondary: ['Inter', 'sans-serif', ...defaultTheme.fontFamily.sans],
+		},
+		extend: {
+      // TODO: remove if not needed
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+			},
+			borderRadius: {
+				'4xl': '2rem'
+			},
+			aspectRatio: {
+				'11/9': '11 / 9',
+			},
+		},
+	},
+	plugins: [],
 }
