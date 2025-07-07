682d13467a902ce863954b0a934381e6d8f0ff26
DubblerPro <igorshkuro2000@gmail.com>
2025-07-07 12:16:33 +0300

    Configured variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,43 +2,52 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: { // TODO: set your containers
-		screens: {
-			sm: '640px',
-			md: '768px',
-			lg: '992px',
-			xl: '1160px',
-		},
-		container: {
-			center: true,
-			padding: '1rem'			
-		},
-		colors: {
-			'white': '#ffffff',
-			'black': '#000000',
-			'red': {
-				DEFAULT: '#fd6150',
-				'error': '#e54230'
-			},
-		},
-		fontFamily: {
-			primary: ['Anek Latin', 'sans-serif', ...defaultTheme.fontFamily.sans], // TODO: include 2 fonts, https://transfonter.org/
-			// secondary: ['Inter', 'sans-serif', ...defaultTheme.fontFamily.sans],
-		},
-		extend: {
-      // TODO: remove if not needed
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-			},
-			borderRadius: {
-				'4xl': '2rem'
-			},
-			aspectRatio: {
-				'11/9': '11 / 9',
-				'4/11': '4 / 11',
-			},
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: { 
+    screens: {
+      sm: '320px',
+      lg: '1240px',
+      xl: '1920px',
+    },
+    container: {
+      center: true, 
+      padding: '0', 
+      screens: {
+        DEFAULT: '100%', 
+        sm: '100%', 
+        lg: '1240px', 
+        xl: '1920px', 
+      },
+        padding: {
+        DEFAULT: '1rem', 
+        lg: '0rem',      
+  },
+    },
+    colors: {
+      'primary-green': '#01A49A',
+      'primary-orange': '#F9AE43',
+      'primary-turquoise': '#EAFFFE',
+      'primary-sand': '#FFD292',
+      'gray': '#F6F6F6',
+      'white': '#FFFFFF',
+      'black': '#1D1D1D',
+      'red-error': '#FF5C5C',
+      'link-blue': '#1983FE',
+    },
+    fontFamily: {
+      primary: ['Montserrat', 'sans-serif', ...defaultTheme.fontFamily.sans],
+      secondary: ['Montserrat Alternates', 'sans-serif', ...defaultTheme.fontFamily.sans]
+    },
+    extend: {
+      padding: {
+        'nav-top': '.4rem',
+        'nav-btm': '1.4rem'
+      },
+      aspectRatio: {
+        '11/9': '11 / 9',
+        '4/11': '4 / 11',
+      },
+    },
+  },
+  plugins: [],
 }
