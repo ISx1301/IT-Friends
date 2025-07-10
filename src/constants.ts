a2f31c9a6949c368f95891062ed93df617b14c0c
DubblerPro <igorshkuro2000@gmail.com>
2025-07-10 18:59:01 +0300

    Added new constant; Connected to index.astro

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
-// Meta
-const SITE_TITLE = 'IT school';
+const SITE_TITLE = 'School IT-Friends';
+const SITE_DESCRIPTION = 'IT school for kids';
 
 export {
-  SITE_TITLE
+  SITE_TITLE,
+  SITE_DESCRIPTION
 }
