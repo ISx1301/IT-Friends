ea3f959341f956321b84518b002db12c49383cf0
Ihor Savvov <ihor.savvov@fedevco.com>
2025-08-27 00:26:17 +0300

    Added animations code sample

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,117 +1,113 @@
-// // import opts from './options';
-// // import sliders from './sliders';
+// import sliders from './sliders';
 
-// const setIntersectionObserver = (section: HTMLElement, callback: Function) => {
-//   const target = section;
+const setIntersectionObserver = (section: HTMLElement, callback: Function) => {
+  const target = section;
 
-//   function handleIntersection(entries: any[]) {
-//     entries.map((entry) => {
-//       if (entry.isIntersecting) {
-//         callback(true, entry);
-//       } else {
-//         callback(false, entry);
-//       }
-//     });
-//   }
+  function handleIntersection(entries: any[]) {
+    entries.map((entry) => {
+      if (entry.isIntersecting) {
+        callback(true, entry);
+      } else {
+        callback(false, entry);
+      }
+    });
+  }
 
-//   const observer = new IntersectionObserver(handleIntersection);
-//   observer.observe(target);
-// }
+  const observer = new IntersectionObserver(handleIntersection);
+  observer.observe(target);
+}
 
-// export interface AppInterface {
-//   sliders?: Object[];
-//   formValidations?: Object;
-//   setObserver: typeof setIntersectionObserver;
-// }
+export interface AppInterface {
+  sliders?: Object[];
+  formValidations?: Object;
+  setObserver: typeof setIntersectionObserver;
+}
 
-// class App implements AppInterface {
+class App implements AppInterface {
 
-//   private _initScriptsEvent!: Event;
-//   sliders: Object[];
-//   formValidations: Object;
-//   setObserver: (section: HTMLElement, callback: Function) => void;
+  private _initScriptsEvent!: Event;
+  sliders: Object[];
+  formValidations: Object;
+  setObserver: (section: HTMLElement, callback: Function) => void;
 
-//   constructor() {
-//     // Vars
-//     this.sliders = [];
-//     this.formValidations = {};
-//     // Funcs
-//     this.setObserver = setIntersectionObserver;
-//     // Calls
-//     this.addEventListeners();
-//     this.init();
-//   }
+  constructor() {
+    // Vars
+    this.sliders = [];
+    this.formValidations = {};
+    // Funcs
+    this.setObserver = setIntersectionObserver;
+    // Calls
+    this.addEventListeners();
+    this.init();
+  }
 
-//   initValidation() {
-//     // this.formValidations = new window.FormValidation(opts.validationOpts);
-//   }
+  initValidation() {
+    // this.formValidations = new window.FormValidation(opts.validationOpts);
+  }
 
-//   initSliders() {
-//     // sliders.forEach((item) => item.mount());
-//     // this.sliders = sliders;
-//   } 
+  initSliders() {
+    // sliders.forEach((item) => item.mount());
+    // this.sliders = sliders;
+  } 
 
-//   watchBlocks(cls = '[data-watch]') {
-//     [...document.querySelectorAll<HTMLElement>(cls)].forEach((el) => {
-//       const isInView = (intersect: boolean) => {
-//         if (intersect) {
-//           setTimeout(() => el.classList.add('is-inview'), 500);
-//         } else if (el.dataset.hasOwnProperty('watchRepeat')) {
-//           setTimeout(() => el.classList.remove('is-inview'), 500);
-//         }
-//       }
-//       this.setObserver(el, isInView);
-//     });
-//   }
+  watchBlocks(cls = '[data-watch]') {
+    [...document.querySelectorAll<HTMLElement>(cls)].forEach((el) => {
+      const isInView = (intersect: boolean) => {
+        if (intersect) {
+          setTimeout(() => el.classList.add('is-inview'), 500);
+        } else if (el.dataset.hasOwnProperty('watchRepeat')) {
+          setTimeout(() => el.classList.remove('is-inview'), 500);
+        }
+      }
+      this.setObserver(el, isInView);
+    });
+  }
 
-//   addEventListeners() {
-//     // eslint-disable-next-line no-underscore-dangle
-//     this._initScriptsEvent = new Event('scriptsInit');
+  addEventListeners() {
+    // eslint-disable-next-line no-underscore-dangle
+    this._initScriptsEvent = new Event('scriptsInit');
 
-//     document.addEventListener('scriptsInit', () => {
-//       console.info('Init scripts');
-//     });
+    document.addEventListener('scriptsInit', () => {
+      console.info('Init scripts');
+    });
 
-//     window.addEventListener('scroll', () => {
+    window.addEventListener('scroll', () => {
 
-//     });
+    });
 
-//     window.addEventListener('load', () => {
-//       document.body.classList.add('is-loaded');
-//       console.log(`${opts.initText}\n`);
-//     });
+    window.addEventListener('load', () => {
+      document.body.classList.add('is-loaded');
+      console.log(`App start\n`);
+    });
 
-//     window.addEventListener('resize', () => {
+    window.addEventListener('resize', () => {
 
-//     });
+    });
 
-//     // Elements events
-//     document.querySelectorAll<HTMLAnchorElement>('a[href*="#"]').forEach((el) => {
-//       el.addEventListener('click', (ev: Event) => {
-//         ev.preventDefault();
-//         const link = new URL(el.href);
-//         const headerHeight = 100;
-//         const target = document.querySelector(link.hash);
-//         if (!target) return;
-//         const y = target.getBoundingClientRect().top + window.scrollY - headerHeight;
-//         window.scroll({
-//           top: y,
-//           behavior: 'smooth'
-//         });
-//       });
-//     });
-//   }
+    // Elements events
+    document.querySelectorAll<HTMLAnchorElement>('a[href*="#"]').forEach((el) => {
+      el.addEventListener('click', (ev: Event) => {
+        ev.preventDefault();
+        const link = new URL(el.href);
+        const headerHeight = 100;
+        const target = document.querySelector(link.hash);
+        if (!target) return;
+        const y = target.getBoundingClientRect().top + window.scrollY - headerHeight;
+        window.scroll({
+          top: y,
+          behavior: 'smooth'
+        });
+      });
+    });
+  }
 
-//   init() {
-//     if (opts.DEBUG) {
-//       console.info('Debug is ON');
-//     }
-//     this.watchBlocks();
-//     // this.initValidation();
-//     this.initSliders();
-//     document.dispatchEvent(this._initScriptsEvent);
-//   }
-// }
+  init() {
+    this.watchBlocks();
+    // this.initValidation();
+    // this.initSliders();
+    document.dispatchEvent(this._initScriptsEvent);
+  }
+}
 
-// const APP = new App();
+new App();
 // window.APP = APP;
